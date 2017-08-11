c91c40e51d45e4aaa92cc9619056ff4bb7ec232b
Benjamin Church <benvoulgarischurch@gmail.com>
2017-08-11 14:21:46 -0400

    added variablity to shape params

--- Monte_Carlo/Monte_Carlo.c
+++ Monte_Carlo/Monte_Carlo.c
@@ -1,24 +1,24 @@
 /*
  * Monte Carlo.c
- * 
+ *
  * Copyright 2017 Benjamin Church <ben@U430>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
 
 
@@ -29,24 +29,26 @@
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_sf_hyperg.h>
 #include <gsl/gsl_sf_gamma.h>
+#include <gsl/gsl_integration.h>
+#include <time.h>
 
 #define min(x,y) (x < y? x : y)
 #define sign(x) (x >= 0? 1.0 : -1.0)
 
 gsl_rng *RNG;
+gsl_integration_workspace * w;
 
 double pi = 3.14159265;
-double crit_density = 1.3211775*0.0000001, 
+double crit_density = 1.3211775*0.0000001,
 	f = 0.1,
 	g = 0.0001,
 	p = 1.9,
-	c = 10.0,
 	G = 0.0045,
 	k = 2,
-	Mprimary = 1000000000000,
+	M_prim = 1000000000000,
 	T_age = 10000;
-	
-double m_max, m_min, expected_num, R_core_prim, R_max_prim;
+
+double m_max, m_min, expected_num, R_core_prim, R_max_prim, c_prim;
 
 double MaxRadius(double M)
 {
@@ -57,60 +59,54 @@ double MaxRadius(double M)
 typedef struct
 {
 	double R, theta;
-	double M, r_core, r_max, alpha, beta, c;
+	double M, r_core, r_max, alpha, beta, c, normalization;
 	double v_r, v_theta, v_phi;
 } halo;
 
-double MFreeNFW(double r, double M)
+double MFreeNFW(double r)
 {
-    double Rmax = MaxRadius(M);
-    double Rc = Rmax/c;
-    if(r < Rmax)
-        return M*(log(1+r/Rc)-r/(r+Rc))/(log(1+c) - c/(1+c));
+    if(r < R_max_prim)
+        return M_prim*(log(1 + r/R_core_prim)-r/(r + R_core_prim))/(log(1 + c_prim) - c_prim/( 1 + c_prim));
     else
-        return M;
+        return M_prim;
 }
 
-double DFreeNFW(double r, double M)
+double DFreeNFW(double r)
 {
-    double Rmax = MaxRadius(M);
-    double Rc = Rmax/c;
-    if(r < Rmax)
-        return 200.0/3.0 * crit_density / (log(1+c) - c/(1+c))*pow(c, 3.0) * 1.0/(r/Rc*pow(1+r/Rc, 2.0));
+    if(r < R_max_prim)
+        return 200.0/3.0 * crit_density / (log(1 + c_prim) - c_prim/(1 + c_prim))*pow(c_prim, 3.0) * 1.0/(r/R_core_prim*pow(1+r/R_core_prim, 2.0));
     else
         return 0.0;
 }
 
-double PhiFreeNFW(double r, double M)
+double PhiFreeNFW(double r)
 {
-    double Rmax = MaxRadius(M);
-    double Rc = Rmax/c;
-    if(r < Rmax)
-        return -M*G*((Rmax/r * log(1+r/Rc) - log(1+c))/(log(1+c) - c/(1+c)) + 1)/Rmax;
+    if(r < R_max_prim)
+        return -M_prim*G*((R_max_prim/r * log(1 + r/R_core_prim) - log(1 + c_prim))/(log(1 + c_prim) - c_prim/(1 + c_prim)) + 1)/R_max_prim;
     else
-        return -M*G/r;
+        return -M_prim*G/r;
 }
 
 double TidalRadius(double M, double R)
 {
-	return R*pow(M/(2*MFreeNFW(R, Mprimary)), 1.0/3.0);
+	return R*pow(M/(2*MFreeNFW(R)), 1.0/3.0);
 }
 
 double func(double x, double r)
 {
-    return (log(1.0+x) - x/(1.0+x))/(log(1.0+c)-c/(1.0+c)) - r;
+    return (log(1.0 + x) - x/(1.0 + x))/(log(1.0 + c_prim) - c_prim/(1.0 + c_prim)) - r;
 }
 
 double df (double x)
 {
-    return x/pow(1.0+x, 2.0) * 1.0/(log(1.0+c)-c/(1.0+c));
+    return x/pow(1.0 + x, 2.0) * 1.0/(log(1.0 + c_prim) - c_prim/(1.0 + c_prim));
 }
 
 double newton(double r)
 {
     int itr, maxmitr = 100;
     double h, x0 = 0.5, x1, allerr = 0.00001;
-    
+
     for (itr = 0; itr < maxmitr; itr++)
     {
         h = func(x0, r)/df(x0);
@@ -125,6 +121,29 @@ double newton(double r)
     return x1;
 }
 
+double rho_profile(double x, void *params)
+{
+	double *param_ptr = (double *)params;
+	double f =  pow(x, 2 - param_ptr[0])*pow((1 + x), param_ptr[0] - param_ptr[1]);
+	return f;
+}
+
+double integ_profile(double alpha, double beta, double r)
+{
+	double result, error;
+	double arr[2];
+	arr[0] = alpha;
+	arr[1] = beta;
+
+  	gsl_function F;
+  	F.function = &rho_profile;
+  	F.params = arr;
+
+  	gsl_integration_qags (&F, 0, r, 0, 1e-6, 1000, w, &result, &error);
+	if(error/result > 1e-6)printf("LARGE INTEG ERROR WARNING %f for result %f", error, result);
+	return result;
+}
+
 double get_R()
 {
 	return newton(gsl_rng_uniform(RNG)) * R_core_prim;
@@ -132,28 +151,34 @@ double get_R()
 
 double get_theta()
 {
-	return acos(2*gsl_rng_uniform(RNG)-1);
+	return acos(2*gsl_rng_uniform(RNG) - 1);
 }
 
 double get_M()
 {
 	double r = gsl_ran_flat(RNG, 0, 1);
-	return Mprimary*pow(pow(m_max/Mprimary, 1.0-p)*r + pow(m_min/Mprimary, 1.0-p)*(1.0-r), 1.0/(1.0-p));
+	return M_prim*pow(pow(m_max/M_prim, 1.0-p)*r + pow(m_min/M_prim, 1.0-p)*(1.0-r), 1.0/(1.0-p));
+}
+
+double c_bar(double M)
+{
+	return pow(10.0, 1.0 - 0.1 * (log10(M) - 12));
 }
 
 void set_shape(halo *ptr, double M, double R)
 {
-	ptr->r_core = pow(3*M/(4 * pi * 200.0 * crit_density), 1.0/3.0)/c;
-	ptr->r_max = min(pow(3*M/(4 * pi * 200.0 * crit_density), 1.0/3.0), TidalRadius(M, R));
-	ptr->alpha = 1.0;
-	ptr->beta = 3.0;
-	ptr->c = c;
+	ptr->r_max = pow(3*M/(4 * pi * 200.0 * crit_density), 1.0/3.0);
+	ptr->alpha = gsl_ran_gaussian_ziggurat(RNG, 0.2) + 1.25; //suggested by J. Ostriker
+	ptr->beta = 3; //probalby need to change
+	ptr->c = gsl_ran_lognormal(RNG, log(c_bar(M)), 0.25); //Ludlow el. al. (2013) and  Frank van den Bosch
+	ptr->r_core = ptr->r_max/ptr->c;
+	ptr->normalization = integ_profile(ptr->alpha, ptr->beta, ptr->c);
 }
 
 void set_velocity(halo *ptr, double R)
 {
-	double v_sigma = sqrt(1.0/3.0)*sqrt(-PhiFreeNFW(R, Mprimary));
-	
+	double v_sigma = sqrt(1.0/3.0)*sqrt(-PhiFreeNFW(R));
+
 	ptr->v_r = gsl_ran_gaussian_ziggurat(RNG, v_sigma);
 	ptr->v_theta = gsl_ran_gaussian_ziggurat(RNG, v_sigma);
 	ptr->v_phi = gsl_ran_gaussian_ziggurat(RNG, v_sigma);
@@ -162,22 +187,37 @@ void set_velocity(halo *ptr, double R)
 double enclosed_mass(halo *ptr, double r)
 {
 	double x = r/(ptr->r_core);
-	double c_halo = ptr->c;
-	if(x > c_halo) 
+	if(x > ptr->c)
 		return ptr->M;
 	else
-		return ptr->M * (log(1+x)-x/(1+x))/(log(1+c_halo) - c_halo/(1+c_halo));
+		return ptr->M * integ_profile(ptr->alpha, ptr->beta, x)/ptr->normalization;
 }
 
 
 void truncate(halo *ptr, double R)
 {
-	double Rt = TidalRadius(ptr->M, R); 
-	double R_max = min(ptr->r_max, Rt);
-	ptr->M = enclosed_mass(ptr, R_max);
-	
-	ptr->r_max = R_max;
-	ptr->c = R_max/ptr->r_core;
+	double l2 = pow(R, 2)*(pow(ptr->v_phi, 2) + pow(ptr->v_theta, 2));
+	double r0 = l2/(M_prim * G);
+	double E = 0.5*(pow(ptr->v_r, 2) + pow(ptr->v_phi, 2) + pow(ptr->v_theta, 2)) + PhiFreeNFW(R);
+	double ecc =  sqrt(1.0 + 2.0*E*l2/pow(M_prim * G, 2));
+	double R_min = min(abs(r0/(1+ecc)), abs(r0/(1-ecc)));
+	//printf("Energy: %f  Reduc: %f\n", E, R_min/R);
+
+	if(E > 0 || R_min < 0.1)
+	{
+		ptr->M = 0;
+	}
+	else
+	{
+		double Rt = TidalRadius(ptr->M, R_min);
+		double R_max = min(ptr->r_max, Rt);
+		double new_M = enclosed_mass(ptr, R_max);
+
+		ptr->r_max = R_max;
+		ptr->c = R_max/ptr->r_core;
+		ptr->normalization *= (new_M/ptr->M);
+		ptr->M = new_M;
+	}
 }
 
 halo *make_halo()
@@ -185,15 +225,15 @@ halo *make_halo()
 	halo *ptr = (halo *) malloc(sizeof(halo));
 	ptr->R = get_R();
 	ptr->theta = get_theta();
-	
+
 	ptr->M = get_M();
 	set_shape(ptr, ptr->M, ptr->R);
 	set_velocity(ptr, ptr->R);
 	truncate(ptr, ptr->R);
-	
+
 	return ptr;
 }
- 
+
 void print_halo_basic(halo *ptr)
 {
 	printf("\n R = %3f \n theta = %3f \n M = %3f \n", ptr->R, ptr->theta, ptr->M);
@@ -239,45 +279,47 @@ int hist(halo **halos, int num_halos, double D, double dD)
 	return sum;
 }
 
-int main(int argc, char **argv)
+void init()
 {
-	
-	//printf("%f\n", log(0.0001));
-	//getchar();
-
 	const gsl_rng_type *T;
-	R_max_prim = MaxRadius(Mprimary);
-	R_core_prim = R_max_prim/c; 
-	m_max = f*Mprimary;
-	m_min = g*Mprimary;
-	expected_num = (2.0-p)/(1.0-p)*f*(pow(m_max/Mprimary, 1.0-p) - pow(m_min/Mprimary, 1.0-p))/(pow(m_max/Mprimary, 2.0-p) - pow(m_min/Mprimary, 2.0-p));
-
 	gsl_rng_env_setup();
-
 	T = gsl_rng_default;
 	RNG = gsl_rng_alloc (T);
-	
-	int num_trials = pow(10, 7);
+	w = gsl_integration_workspace_alloc (1000);
+
+	R_max_prim = MaxRadius(M_prim);
+	c_prim = c_bar(M_prim);
+	R_core_prim = R_max_prim/c_prim;
+	m_max = f*M_prim;
+	m_min = g*M_prim;
+	expected_num = (2.0-p)/(1.0-p)*f*(pow(m_max/M_prim, 1.0-p) - pow(m_min/M_prim, 1.0-p))
+		/ (pow(m_max/M_prim, 2.0-p) - pow(m_min/M_prim, 2.0-p));
+}
+
+int main(int argc, char **argv)
+{
+	init();
+	int num_trials = pow(10, 6);
 	int num_points = 100;
 	double Flucs[num_points], Dens[num_points], Hist[num_points];
 	double mass = 0, avg_mass = 0;
 	int i,j;
-	
+
 	for(i = 0; i < num_points; i++)
 	{
 		Flucs[i] = 0;
 		Dens[i] = 0;
 		Hist[i] = 0;
 	}
-	
+
 	for(i = 0; i < num_trials; i++)
 	{
 		if(i % 100 == 0)printf("%d out of %d \n", i/100, num_trials/100);
 		unsigned long num_halos = gsl_ran_poisson(RNG, expected_num);
-		
+
 		mass = 0;
 		//printf("Expect: %f   Have: %lu \n", expected_num, num_halos);
-		
+
 		halo *halolist[num_halos];
 		for(j = 0; j < num_halos; j++)
 		{
@@ -292,20 +334,20 @@ int main(int argc, char **argv)
 			Dens[j] += H_Density(halolist, num_halos, R_max_prim*j/(double)num_points, R_max_prim/(double)num_points)/num_trials;
 			Hist[j] += hist(halolist, num_halos, R_max_prim*(j+1)/(double)num_points, R_max_prim/(double)num_points)/num_trials;
 		}
-		//printf("Mass frac: %f \n", mass/Mprimary);
+		//printf("Mass frac: %f \n", mass/M_prim);
 		avg_mass += mass/num_trials;
-		
+
 		/*for(j = 0; j < num_points; j++)
 		{
 			//printf("%f : %f\n", pow(10, 5.0*j/num_points), log10(Flucs[j]));
 			//printf("%f : %f\n", pow(10, 5.0*j/num_points), log10(Dens[j]));
 			//printf("%f : %f\n", pow(10, 5.0*j/num_points), Hist[j]);
 		}*/
-		
+
 		for(j = 0; j < num_halos; j++)
 			free(halolist[j]);
 	}
-	
+
 	FILE *f = fopen("Density.txt", "w");
 	if(f != NULL)
 	{
@@ -322,7 +364,7 @@ int main(int argc, char **argv)
 			printf("%f : %f\n", log10(R_max_prim*(j+1)/(double)num_points), log10(Dens[j]));
 		}
 	}
-	
+
 	f = fopen("Flucs.txt", "w");
 	if(f != NULL)
 	{
@@ -332,7 +374,7 @@ int main(int argc, char **argv)
 		}
 		fclose(f);
 	}
-	
+
 	else
 	{
 		for(j = 0; j < num_points; j++)
@@ -341,8 +383,8 @@ int main(int argc, char **argv)
 		}
 	}
 
-	printf("Mass frac: %f \n", avg_mass/Mprimary);
+	printf("Mass frac: %f \n", avg_mass/M_prim);
 	gsl_rng_free (RNG);
+	gsl_integration_workspace_free (w);
 	return 0;
 }
-
--- Monte_Carlo/plots.py
+++ Monte_Carlo/plots.py
@@ -8,22 +8,22 @@ from matplotlib import pyplot as plt
 import time
 
 
-params = { 
-   'axes.labelsize': 24, 
-   'axes.titlesize': 22,  
-   'legend.fontsize': 20, 
-   'xtick.labelsize': 24, 
-   'ytick.labelsize': 24, 
+params = {
+   'axes.labelsize': 24,
+   'axes.titlesize': 22,
+   'legend.fontsize': 20,
+   'xtick.labelsize': 24,
+   'ytick.labelsize': 24,
    'text.usetex': True,
    'figure.figsize': [10,8], # instead of 4.5, 4.5
    'lines.linewidth': 2,
-   'xtick.major.pad': 15, 
-   'ytick.major.pad': 15, 
+   'xtick.major.pad': 15,
+   'ytick.major.pad': 15,
    'figure.subplot.bottom': 0.12,
    'figure.subplot.top': 0.95,
    'figure.subplot.left': 0.225,
    #'font.size': 22
-   }   
+   }
 plt.rcParams.update(params)
 
 def readData(fileName):
@@ -34,32 +34,29 @@ def readData(fileName):
 		if "nan" in l or "inf" in l:
 			to_remove.append(l)
 			print "Remoivng: " + str(l)
-	
+
 	for l in to_remove:
 		lines.remove(l)
-		
+
 	x, y = list(map(lambda x : 10**float(x.split(" ")[0]), lines)), list(map(lambda x : 10**float(x.split(" ")[1]), lines))
 	file_obj.close()
-	return x, y 
+	return x, y
 
-def main():	
+def main():
 	Dens_x, Dens_y = readData("Density.txt")
 	Flucs_x, Flucs_y = readData("Flucs.txt")
-	
+
 	plt.figure()
 	plt.loglog(Dens_x, Dens_y)
 	plt.xlabel(r'$ r(pc)$')
 	plt.ylabel(r'$ \rho \quad (M_{\odot} pc^{-3})$')
-	
+
 	plt.figure()
-	plt.plot(Flucs_x, Flucs_y)
+	plt.loglog(Flucs_x, Flucs_y)
 	plt.xlabel(r'$ r(pc)$')
 	plt.ylabel(r'$ \sqrt{\left < \left (\frac{\partial \phi}{\partial t} \right )^2 \right >} \quad ((km/s)^{2} Myr^{-1})$')
 	plt.show()
-	
-    
+
+
 if __name__ == "__main__":
     main()
-
-    
-
