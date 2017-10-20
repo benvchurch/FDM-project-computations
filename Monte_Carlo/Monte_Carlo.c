c842542eed34d2a10e8d1412d63e858d6642abca
Ben Church <benvoulgarischurch@gmail.com>
2017-10-20 15:11:35 -0400

    added cutoff scale

--- Monte_Carlo/Monte_Carlo.c
+++ Monte_Carlo/Monte_Carlo.c
@@ -36,6 +36,7 @@
 #define INTEGRATION_POINTS 81
 #define INTEGRAL_FUDGE_FACTOR 1.0125
 #define INTEGRATION_SECTIONS 8
+#define CUTOFF_SCALE 0.1
 #define min(x,y) (x < y? x : y)
 #define sign(x) (x >= 0? 1.0 : -1.0)
 #define dot_macro(A, B) ((A).x * (B).x + (A).y * (B).y + (A).z * (B).z)
@@ -436,7 +437,7 @@ double Fluc(halo *halos, int num_halos, double D)
 		make_unit(diff)
 		double v_r = dot_macro(halos[i].v, diff);
 
-		sum += enclosed_mass(halos + i, r) * G /(r*r) * v_r;
+		sum += r < CUTOFF_SCALE ? (enclosed_mass(halos + i, r) * G /(r*r) * v_r) : 0;
 		/*printf("%.3f\n", D);
 		printf("velocity: ");
 		print_vector(halos[i]->v);
